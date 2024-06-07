e23e58edf030407889ce9c188b2fdecacc62c5fd
unknown <duydepz41@gmail.com>
2024-06-07 10:21:59 +0700

    Add contract and create Vite for frontend

---
+++ client/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import { ThirdwebProvider } from "thirdweb/react";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThirdwebProvider>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
---
+++ web3/hardhat.config.js
@@ -0,0 +1,20 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork: 'sepolia',
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: 'https://sepolia.drpc.org',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
